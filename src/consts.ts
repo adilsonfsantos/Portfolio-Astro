e82bc10433a6905fd73ee70caeaeb8ead9e99c2d
Adilson Santos <adilson.santos97@live.com>
2023-08-23 18:44:39 -0300

    Replaced global data

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,31 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+//export const SITE_TITLE = 'Astro Blog';
+//export const SITE_DESCRIPTION = 'Welcome to my website!';
+
+export const site_name =  "Adilson Santos";
+export const site_title =  "Adilson Santos";
+export const site_url =  "https://adilsonsantos.netlify.app";
+export const site_lang =  "pt-BR";
+export const site_description =  "Meu nome é Adilson Santos e eu sou um designer focado em identidades visuais, UI e UX.";
+export const site_image =  "https://adilsonsantos.netlify.app/assets/images/thumbnail.jpg";
+export const site_author = "Adilson Santos";
+export const site_author_email = "adilson.design@outlook.com"
+
+export const social = {
+  "links": [
+    {
+      "name": "Behance",
+      "url": "https://behance.net/adilsonfsantos"
+    },
+    {
+      "name": "Github",
+      "url": "https://github.com/adilsonfsantos"
+    },
+    {
+      "name": "LinkedIn",
+      "url": "https://linkedin.com/in/adilsonfsantos"
+    }
+  ]
+}
