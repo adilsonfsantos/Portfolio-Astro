eeca104690e2203907d0f0a6d5345c963826bfa9
Adilson Santos <adilson.santos97@live.com>
2023-08-23 18:46:04 -0300

    Replaced "blog" collection to "projetos"

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,21 +1,20 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
-	// Type-check frontmatter using a schema
+const projetos = defineCollection({
+	type: "content",
 	schema: z.object({
 		title: z.string(),
 		description: z.string(),
-		// Transform string to Date object
-		pubDate: z
-			.string()
-			.or(z.date())
-			.transform((val) => new Date(val)),
-		updatedDate: z
-			.string()
-			.optional()
-			.transform((str) => (str ? new Date(str) : undefined)),
-		heroImage: z.string().optional(),
-	}),
-});
+		date: z
+		.string()
+		.or(z.date())
+		.transform((val) => new Date(val)),
+	image: z.string().optional(),
+	isDraft: z.boolean().default(false),
+	categories: z.string().array(),
+	})
+})
 
-export const collections = { blog };
+
+export const collections = { "projetos": projetos };
+
