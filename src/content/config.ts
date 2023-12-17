dc533cf579b3e2593f1f1c32da5cac1958e56620
Adilson Santos <adilson.santos97@live.com>
2023-12-16 23:31:12 -0300

    Refactor schema to handle markdown frontmatter

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,19 +2,24 @@ import { defineCollection, z } from 'astro:content';
 
 const projetos = defineCollection({
 	type: "content",
-	schema: z.object({
+	schema: ({ image }) => z.object({
 		title: z.string(),
 		description: z.string(),
 		date: z
 		.string()
 		.or(z.date())
 		.transform((val) => new Date(val)),
-	image: z.string().optional(),
-	isDraft: z.boolean().default(false),
-	categories: z.string().array(),
-	})
+  	image: image(),
+		isDraft: z.boolean().default(false),
+		categories: z.string().array(),
+		roles: z.string().array(),
+		team: z.optional(z.array(z.object({
+			name: z.string(),
+			portfolio: z.optional(z.string()),
+			role: z.string()
+		})))
+  })
 })
-
 
 export const collections = { "projetos": projetos };
 
