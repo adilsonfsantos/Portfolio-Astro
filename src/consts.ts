4e7d2fd3a8f7e9e42daaa41299d7cc845a9d6168
Adilson Santos <adilson.santos97@live.com>
2023-12-16 23:32:58 -0300

    Removed commented unnecessary lines

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-//export const SITE_TITLE = 'Astro Blog';
-//export const SITE_DESCRIPTION = 'Welcome to my website!';
-
 export const site_name =  "Adilson Santos";
 export const site_title =  "Adilson Santos";
 export const site_url =  "https://adilsonsantos.netlify.app";
