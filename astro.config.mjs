aa75def971d0946eda08e3cc2bc1e4c4a3632b34
Adilson Santos <adilson.santos97@live.com>
2024-02-26 00:58:45 -0300

    Removed redundant prefetch
    Currently the prefetch option is stating the same values from the
    default, so is better to remove the values.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig, sharpImageService } from "astro/config";
-
 import lighthouse from "astro-lighthouse";
 
 // https://astro.build/config
 export default defineConfig({
-  prefetch: true,
   site: "https://adilsonsantos.netlify.app/",
   integrations: [mdx(), sitemap(), lighthouse()],
   image: {
